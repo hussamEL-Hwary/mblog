5ea4261e338e36ce777a8f2f937344e43636d6b9
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2017-07-22 15:15:08 +0200

    comments model and controller

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+use App\Comments;
+class CommentsController extends Controller
+{
+    public function store(Post $post)
+    {
+      Comments::create([
+        'body'=>request('body'),
+        'user_id'=>auth()->id(),
+        'post_id'=>$post->id
+      ]);
+      return back();
+    }
+}
