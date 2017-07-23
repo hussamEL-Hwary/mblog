abc9c94e9ae1a11c11a687447fa680529ad1b522
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2017-07-23 13:13:08 +0200

    add archives function for side bar

--- app/Post.php
+++ app/Post.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Carbon\Carbon;
 class Post extends Model
 {
       protected $guarded=[];
@@ -16,5 +16,27 @@ class Post extends Model
     public function user()
     {
       return $this->belongsTo(User::class);
+    }
+
+    public function scopeFilter($query,$filters)
+    {
+      if($month=$filters['month'])
+      {
+        $query->whereMonth('created_at', Carbon::parse($month)->month);
+      }
+
+      if($year=$filters['year'])
+      {
+        $query->whereYear('created_at',$year);
+      }
+
+    }
+    public static function archives()
+    {
+      return static::selectRaw('year(created_at)year,monthname(created_at)month,count(*)published')
+      ->groupBy('year','month')
+      ->orderByRaw('min(created_at) desc')
+      ->get()
+      ->toArray();
     }
 }
