70ed6efa6df4233971b1f9f1b86129b91cb63453
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2017-07-26 15:19:40 +0200

    mark mile

--- resources/views/mails/welcomeUser.blade.php
+++ resources/views/mails/welcomeUser.blade.php
@@ -1,16 +1,12 @@
 @component('mail::message')
-# Introduction
-
-#welcome to Competitive Programming blog :)
+#welcome {{ $user->name }} to Competitive Programming blog :)
 
 @component('mail::button', ['url' => ''])
 see Posts
 @endcomponent
 
-@component('mail::panel', ['url' => ''])
-just for test
-@endcomponent
+
 
 Thanks,<br>
-{{ config('app.name') }}
+{{ config('app.name') }} team
 @endcomponent
