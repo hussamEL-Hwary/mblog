67319d8a092ae131ebebb226cbce7f10f96ea76a
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2017-07-26 15:13:18 +0200

    view flash message to user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,10 @@
 
 @section('content')
 
-<a class="btn btn-outline-primary" href="/posts/create">Create Post</a>
+<a class="btn btn-primary" href="/posts/create">Create Post</a> <br><br>
+
   @foreach($posts as $post)
       @include('posts.posts')
   @endforeach
-
-  <nav class="blog-pagination">
-        <a class="btn btn-outline-primary" href="#">Older</a>
-        <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
-  </nav>
 
 @endsection
