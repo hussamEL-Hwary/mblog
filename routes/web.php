9d976fd4d583d7040d0e2d1c79d886d51639b7c3
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2017-07-26 15:20:58 +0200

    get things ready

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,10 @@
 <?php
 
 
-//Route::get('/','PostController@index');
+Route::get('/','PostController@index');
 Route::get('/posts/create','PostController@create');
 Route::get('/posts/{post}','PostController@show');
 Route::post('/posts/{post}/comment','CommentsController@store');
 Route::post('/posts','PostController@store');
-
-
-
-
-Route::get('/', function () {
-    return view('layouts.master');
-});
-
 Auth::routes();
-
 Route::get('/home', 'PostController@index')->name('home');
